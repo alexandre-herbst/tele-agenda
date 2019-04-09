7bfcc684e46def10b098cb715a4433255f381bda
Alexandre Herbst <alexandre.hc@aluno.ifsc.edu.br>
2019-04-09 16:37:25 -0300

    Classes criadas com base no diagrama UML

---
+++ src/main/java/poo/Telefone.java
@@ -0,0 +1,35 @@
+package poo;
+
+import java.util.HashMap;
+
+public class Telefone {
+
+
+     // Declarações dos atributos e construtor:
+
+    public Telefone(){ }
+    private HashMap<String, String> dados = new HashMap<>();
+
+
+    // Declaração dos metodos
+
+    public boolean add(String r, String n){
+
+    }
+
+    public boolean remove(String r){
+
+    }
+    public  boolean update(String r, String n){
+
+    }
+
+    @Override
+    public String toString() {
+
+
+    }
+
+
+
+}
